9be8a1d66f90efb8674318b39eda162381f673e1
Fatima <Fatemeh.aghapourasl@gmail.com>
2024-01-28 14:06:17 -0500

    Update layered_model.py

--- layered_model.py
+++ layered_model.py
@@ -1,24 +1,40 @@
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow import keras
-import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.losses import BinaryCrossentropy
+import tensorflow as tf
+
+# ... [rest of your functions]
+
+
 
 def define_dense_model_single_layer(input_length, activation_f='sigmoid', output_length=1):
-    model = keras.Sequential([Dense(output_length, input_shape=(input_length,), activation=activation_f)])
+    """Define a dense model with a single layer with the following parameters:
+    input_length: the number of inputs
+    activation_f: the activation function
+    output_length: the number of outputs (number of neurons)"""
+    model = keras.Sequential([
+        layers.Dense(output_length, activation=activation_f, input_shape=(input_length,))
+    ])
     return model
 
 def define_dense_model_with_hidden_layer(input_length, 
                                          activation_func_array=['relu','sigmoid'],
                                          hidden_layer_size=10,
                                          output_length=1):
-    model =keras.Sequential([Dense(hidden_layer_size,input_shape=(input_length,),activation=activation_func_array[0]),
-                        Dense(output_length,activation= activation_func_array[1])
-                       ])
+    """Define a dense model with a hidden layer with the following parameters:
+    input_length: the number of inputs
+    activation_func_array: the activation function for the hidden layer and the output layer
+    hidden_layer_size: the number of neurons in the hidden layer
+    output_length: the number of outputs (number of neurons in the output layer)"""
+
+    model = keras.Sequential([
+        layers.Dense(hidden_layer_size, activation=activation_func_array[0], input_shape=(input_length,)),
+        layers.Dense(output_length, activation=activation_func_array[1])
+    ])
+    return model
     return model
 
 
@@ -35,12 +51,22 @@ def binarize_labels(labels, target_digit=2):
     return labels
 
 def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=10, batch_size=128):
+    """Fit the model to the data.
+    compile the model and add parameters for  the "optimizer", the loss function , 
+    and the metrics, Hint: use binary crossentropy for the loss function .
+
+    then fit the model on the training data. (pass the epochs and batch_size params)
+    """
     y_train = binarize_labels(y_train, target_digit)
+
     model.compile(optimizer='adam', loss=BinaryCrossentropy(), metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=epochs , batch_size=batch_size)
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
     return model
 
 def evaluate_mnist_model_single_digit(x_test, y_test, target_digit, model):
+    """Evaluate the model on the test data.
+    Hint: use model.evaluate() to evaluate the model on the test data.
+    """
     y_test = binarize_labels(y_test, target_digit)
     loss, accuracy = model.evaluate(x_test,y_test)
     return loss, accuracy
